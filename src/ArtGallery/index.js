1e49c9ae1126a098cf5d67d7a4451e932d162b8c
Minh Pham <mqpham848@gmail.com>
2019-10-23 23:51:54 -0400

    added card css and got img to mount

--- src/ArtGallery/index.js
+++ src/ArtGallery/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './card.css';
 
-function ArtGallery (props) {
+function ArtGallery(props) {
   return (
-    <div className='card'> 
+    <div className='card'>
       <div className='img-container'>
-        <img className='painting' alt={props.artist} src={props.image} onClick={() => props.handleClickedImage(props.id)} />
+        <img
+          className='painting' 
+          alt={props.title}
+          src={props.image}
+          onClick={() => props.handleClickedImage(props.id)}
+        />
       </div>
-      <div className='overlay'>
-        <div className='text'>
-          {props.title}
-          <br />
-          by
-          <br />
-          {props.artist} in {props.year}
-        </div>
+      <div className='content'>
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.title}
+          </li>
+          <li>
+            <strong>Artist:</strong> {props.artist}
+          </li>
+          <li>
+            <strong>Year:</strong> {props.year}
+          </li>
+        </ul>
       </div>
     </div>
   );
